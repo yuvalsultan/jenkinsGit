c9d73278a997fd5122fc0644dcc0a149a05fe552
yuval <yuval.sultan@gmail.com>
2023-05-23 13:12:35 +0300

    second commit

---
+++ yuval.java
@@ -0,0 +1,9 @@
+public class PrintName {
+    public static void main(String[] args) {
+        int repetitionCount = 5; // Set the number of times to repeat the name
+
+        for (int i = 0; i < repetitionCount; i++) {
+            System.out.println("Yuval Sultan");
+        }
+    }
+}
